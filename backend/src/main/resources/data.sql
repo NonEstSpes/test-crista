bb7496eb1ead879c1f2f3af9a074356f5203a768
Bobylev E. <102691182+NonEstSpes@users.noreply.github.com>
2025-06-27 21:48:01 +0300

    feature: добавление моковых данных при запуске backend

---
+++ backend/src/main/resources/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO book(title, author, year) VALUES ('Война и мир', 'Лев Толстой', 1869);
+INSERT INTO book(title, author, year) VALUES ('Преступление и наказание', 'Фёдор Достоевский', 1866);
